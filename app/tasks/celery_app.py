102d787455673ea415c683aeb9f9df644b91a946
val <vklenovskii@gmail.com>
2025-11-09 17:45:09 +0300

    n clry

--- app/tasks/celery_app.py
+++ app/tasks/celery_app.py
@@ -1,12 +1,12 @@
-from celery import Celery
-from app.config import settings
-from app.tasks.celery_config import CELERY_BEAT_SCHEDULE
-
-celery_app = Celery(
-    "tasks",
-    broker=settings.CELERY_BROKER_URL,
-    backend=settings.CELERY_RESULT_BACKEND,
-)
-
-celery_app.conf.beat_schedule = CELERY_BEAT_SCHEDULE
-celery_app.autodiscover_tasks(["app.tasks"])
+# from celery import Celery
+# from app.config import settings
+# from app.tasks.celery_config import CELERY_BEAT_SCHEDULE
+#
+# celery_app = Celery(
+#     "tasks",
+#     broker=settings.CELERY_BROKER_URL,
+#     backend=settings.CELERY_RESULT_BACKEND,
+# )
+#
+# celery_app.conf.beat_schedule = CELERY_BEAT_SCHEDULE
+# celery_app.autodiscover_tasks(["app.tasks"])
--- app/tasks/celery_config.py
+++ app/tasks/celery_config.py
@@ -1,9 +1,9 @@
-from celery.schedules import crontab
-
-CELERY_BEAT_SCHEDULE = {
-    "update-player-stats-hourly": {
-        "task": "update_player_stats",
-        "schedule": crontab(minute=0),
-        "args": (116,),
-    },
-}
+# from celery.schedules import crontab
+#
+# CELERY_BEAT_SCHEDULE = {
+#     "update-player-stats-hourly": {
+#         "task": "update_player_stats",
+#         "schedule": crontab(minute=0),
+#         "args": (116,),
+#     },
+# }
