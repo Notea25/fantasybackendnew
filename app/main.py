716b9b99ab36768799b3779663338fa06beea38c
val <vklenovskii@gmail.com>
2025-11-17 20:31:19 +0300

    n clry

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], #dev mode
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
